0add335724859292578ddac30d300182cf3c5b7d
Amit Kumar <toamitkumar@gmail.com>
2015-01-17 13:21:18 +0530

    Added routes for meetup API

--- routes.js
+++ routes.js
@@ -1,9 +1,24 @@
 /* jslint node: true */
 'use strict';
 
-var Static = require('./static');
+var Static = require('./static'),
+		meetup = require('./lib/controllers/meetup')
 
 // API Server Endpoints
 exports.endpoints = [
-	{ method: 'GET',  path: '/{params*}', config: Static.get }
+	{ method: 'GET',  
+		path: '/{params*}', 
+		config: Static.get },
+	{ method: 'GET', 
+		path: '/api/meetups', 
+		config: {
+			handler: meetup.meetups
+		}
+	},
+	{ method: 'GET', 
+		path: '/api/meetups/{meetup_id}', 
+		config: {
+			handler: meetup.details
+		}
+	}
 ];
