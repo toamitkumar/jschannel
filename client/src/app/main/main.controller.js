ce8974bedfc03671a8206d50976e9f23323596d2
Amit Kumar <toamitkumar@gmail.com>
2015-02-13 19:12:45 +0530

    Better modal and updated text on the page

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -15,7 +15,7 @@ angular.module('jschannel')
   	$scope.members = [
   		{ name: 'Amit Anand', twitter: 'https://twitter.com/iamitanand', facebook: 'https://www.facebook.com/aanand388', linkedin: 'http://au.linkedin.com/in/iamitanand', title: 'Digital Engagement Manager'},
   		{ name: 'Amit Kumar', twitter: 'https://twitter.com/toamit', facebook: 'https://www.facebook.com/toamitkumar', linkedin: 'https://www.linkedin.com/in/toamitkumar', title: 'Digital Expert'},
-  		{ name: 'Naresh Sharma', twitter: '', facebook: '', linkedin: '', title: ''},
+  		{ name: 'Naresh Sharma', twitter: 'https://twitter.com/narunaram', facebook: 'https://www.facebook.com/naresh.sharma.798', linkedin: 'http://in.linkedin.com/in/narunaram', title: 'Digital Consultant'},
   		{ name: 'Niraj Bhandari', twitter: 'https://twitter.com/nirajkbhandari ', facebook: 'https://www.facebook.com/nirajkbhandari', linkedin: 'http://in.linkedin.com/in/nirajbhandari', title: 'Product Manager'},
   		{ name: 'Suman Paul', twitter: 'https://twitter.com/sumankpaul', facebook: 'https://www.facebook.com/paul.sumank', linkedin: 'https://in.linkedin.com/in/skeep/', title: 'Senior Digital Analyst'}
   	];
@@ -28,7 +28,7 @@ angular.module('jschannel')
       switch(which) {
         case 'conference': 
           modalInstance = $modal.open({
-            templateUrl: 'components/modals/conference.html',
+            templateUrl: 'components/modals/conference1.html',
             controller: 'ModalInstanceCtrl',
             size: 'lg'
           });
